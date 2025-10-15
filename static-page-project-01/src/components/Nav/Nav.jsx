9c83a10cc566679a9b4d0f83c6578a32f19c6efe
Josh Lee <jleec@yorksolutions.net>
2025-10-15 08:57:08 -0500

    added Navbar and Dashboard

---
+++ static-page-project-01/src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+function Navbar(){
+    return (
+        <>
+            <header>
+                <img src="../../assets/react.svg" width={40} />
+                <span>React App</span>
+            </header>
+        </>
+    )
+}
+
+export default Navbar
---
+++ static-page-project-01/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/nav/Nav.jsx"
+
+function Dashboard(){
+    return (
+        <>
+            <Navbar />
+            <main>
+                <h1>This is a header</h1>
+            </main>
+        </>
+    )
+}
+
+export default Dashboard
