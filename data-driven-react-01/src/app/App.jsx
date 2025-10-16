e0e2624815732cbcaee466634bc66eafe936ef2c
Josh Lee <jleec@yorksolutions.net>
2025-10-15 19:56:38 -0500

    added the Entry component

--- data-driven-react-01/src/app/App.jsx
+++ data-driven-react-01/src/app/App.jsx
@@ -1,9 +1,14 @@
-import Header from "@/components/header/Header"
+import Entry from "@/components/entry/Entry.jsx"
+import Header from "@/components/header/Header.jsx"
+import "./App.css"
 
 export default function App(){
   return(
     <>
       <Header />
+      <main className="container">
+        <Entry />
+      </main>
     </>
   )
 }
