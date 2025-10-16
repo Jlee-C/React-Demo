bd80f466206224c43cd38f3251c69d06d38ffde9
Josh Lee <jleec@yorksolutions.net>
2025-10-16 09:30:43 -0500

    added data.js to import contents into entry components

--- data-driven-react-01/src/app/App.jsx
+++ data-driven-react-01/src/app/App.jsx
@@ -1,13 +1,27 @@
 import Entry from "@/components/entry/Entry.jsx"
 import Header from "@/components/header/Header.jsx"
 import "./App.css"
+import entryData from "@/data.js"
 
 export default function App(){
+
+  const entryElements = entryData.map((entry) =>
+
+    <Entry img={entry.img}
+    country={entry.country}
+    title={entry.title}
+    googleMapsLink={entry.googleMapsLink}
+    dates={entry.dates}
+    text={entry.text}
+    />
+
+)
+
   return(
     <>
       <Header />
       <main className="container">
-        <Entry />
+        {entryElements}
       </main>
     </>
   )
