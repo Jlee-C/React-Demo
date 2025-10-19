47c9fe43562ff65ee85cbb3b3aebc28beefec725
Josh Lee <jleec@yorksolutions.net>
2025-10-19 15:03:00 -0500

    updated README.md

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ There are currently three projects I have finished:
 - static-page-project-01
 - static-page-project-02
 - data-driven-react-01
+- react-state-project-01
--- react-state-project-01/src/components/recipe/Recipe.jsx
+++ react-state-project-01/src/components/recipe/Recipe.jsx
@@ -1,15 +1,8 @@
+import ReactMarkdown from "react-markdown";
+
 export default function Recipe({ recipe }) {
   recipe
     ? console.log("Recipe shold be showing")
     : console.log("Recipe should not be showing");
-  return (
-    <>
-      {recipe ? (
-        <section>
-          {recipe}
-          <p>Is Showing</p>
-        </section>
-      ) : null}
-    </>
-  );
+  return <>{recipe ? <ReactMarkdown children={recipe} /> : null}</>;
 }
