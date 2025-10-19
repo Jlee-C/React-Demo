254fb1bac652a4fecf832847ea8b600e6b1d9792
Josh Lee <jleec@yorksolutions.net>
2025-10-19 15:22:08 -0500

    template web page added

---
+++ side-effects-project-01/src/components/header/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="https://tenor.com/view/trollface-gif-17281652918473763503" />
+      <h1>Meme Generator</h1>
+    </header>
+  );
+}
---
+++ side-effects-project-01/src/main.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/header/Header.jsx";
+import App from "./App.jsx";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <Header />
+    <App />
+  </>
+);
